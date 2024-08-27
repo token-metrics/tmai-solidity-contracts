a2ee66f7481ee189cc94d1154870bf04ba504dda
manav650 <manavgarg650@gmail.com>
2024-08-27 15:18:13 +0530

    updated contracts

--- src/TMAI-vesting.sol
+++ src/TMAI-vesting.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.2;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
